341afca9547562a29110efa91c9b3a5a48fd571f
TaceyWong <xinyong.wang@qq.com>
2016-07-27 20:36:38 +0800

    整理

--- spider_learn/1.py
+++ spider_learn/1.py
@@ -1,13 +1,29 @@
-#coding:utf-8
-__author__ = 'Administrator'
+# coding:utf-8
 
-"""
-基础内容：
+import urllib2
+print "test"
 
-①：Python基础知识
-②：urllib、urllib2、requests、HTTP协议相关及周边
-③：正则表达式、BS、JSON、xml、CSV
-④：Scrapy
-⑤：多线程、多进程、数据库、数据管道
+#urlopen(url,data,timeout)
+#timeout = socket._GLOBAL_DEFAULT_TIMEOUT
+response = urllib2.urlopen("http://www.baidu.com")
+print response.read()
 
-"""
+request = urllib2.Request("http://www.baidu.com")
+response = urllib2.urlopen(request)
+print response.read()
+
+import urllib
+
+values = {
+    "input1":"tacey",
+    "input2":"xxxxxx",
+    "remember":"true"
+    }
+data = urllib.urlencode(values)
+url = "http://passport.cnblogs.com/user/signin"
+data = urllib.urlencode(values)
+request = urllib2.Request(url,data)
+response = urllib2.urlopen(request)
+
+print response.read().decode("utf-8")
+
